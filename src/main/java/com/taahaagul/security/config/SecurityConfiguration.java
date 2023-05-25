5c7afed595af4dd9ed3abadaf88f32565b4041c4
TaahaaGul <tahagulll06@gmail.com>
2023-05-25 18:57:09 +0300

    updated expire status codes

--- src/main/java/com/taahaagul/security/config/SecurityConfiguration.java
+++ src/main/java/com/taahaagul/security/config/SecurityConfiguration.java
@@ -22,11 +22,8 @@ public class SecurityConfiguration {
     private final LogoutHandler logoutHandler;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, CustomAuthenticationEntryPoint authenticationEntryPoint) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .exceptionHandling()
-                .authenticationEntryPoint(authenticationEntryPoint)
-                .and()
                 .cors()
                 .and()
                 .csrf()
